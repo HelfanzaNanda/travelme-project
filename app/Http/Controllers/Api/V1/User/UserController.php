a653e7c56fb369fc70dad1c825dcbe78a3cfefb9
helfanzananda <elvannnd@gmail.com>
2020-05-25 16:50:29 +0700

    coba midtrans

--- app/Http/Controllers/Api/V1/User/UserController.php
+++ app/Http/Controllers/Api/V1/User/UserController.php
@@ -45,23 +45,13 @@ class UserController extends Controller
     {
         $this->request = $request;
 
-        Config::$serverKey = 'SB-Mid-server-lgheMLSAsWyuFmE1FmP7L2K1';
-        if (!isset(Config::$serverKey)) {
-            return "Please set your payment server key";
-        }
-        Config::$isSanitized = true;
-
-        // Enable 3D-Secure
-        Config::$is3ds = true;
-
-
-        //Midtrans::$serverKey = 'SB-Mid-server-lgheMLSAsWyuFmE1FmP7L2K1';
+        Midtrans::$serverKey = 'SB-Mid-server-lgheMLSAsWyuFmE1FmP7L2K1';
         //Config::$serverKey = 'SB-Mid-server-lgheMLSAsWyuFmE1FmP7L2K1';
         //Config::$isSanitized = true;
         //Config::$is3ds = true;
 
         //Midtrans::$serverKey = config('services.midtrans.serverKey');
-        //Midtrans::$isProduction = false;
+        Midtrans::$isProduction = false;
         $this->middleware('auth:api')->except(['getUsers', 'getUserLogIn']);
     }
 
@@ -298,16 +288,16 @@ $payload = [
                 ],
             ];
 
-            $snapToken = Snap::getSnapToken($transaction);
+            //$snapToken = Snap::getSnapToken($transaction);
                 $snap = Midtrans::getSnapToken($payload);
                 return response()->json([
                     'message' => 'successfully get snap',
                     'status' => true,
-                    'data' => $snapToken
-                    /*'data' => [
+                    //'data' => $snapToken
+                    'data' => [
                         'token' => $snap,
                         'redirect_url' => Midtrans::getSnapBaseUrl().'/'.$snap
-                    ],*/
+                    ],
                 ]);
             } catch (\Exception $e) {
                 return response()->json([
