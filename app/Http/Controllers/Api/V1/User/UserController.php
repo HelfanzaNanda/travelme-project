884ec17b769931087c2adf900831c4de789ccae6
helfanzananda <elvannnd@gmail.com>
2020-05-25 17:56:50 +0700

    coba midtrans

--- app/Http/Controllers/Api/V1/User/UserController.php
+++ app/Http/Controllers/Api/V1/User/UserController.php
@@ -346,6 +346,84 @@ $payload = [
         }
     }
 
+
+    public function getSnapToken(Request $req){
+
+        $item_list = array();
+        $amount = 0;
+        // Required
+
+         $item_list[] = [
+                'id' => "111",
+                'price' => 20000,
+                'quantity' => 1,
+                'name' => "Majohn"
+        ];
+
+        $transaction_details = array(
+            'order_id' => rand(),
+            'gross_amount' => 20000, // no decimal allowed for creditcard
+        );
+
+
+        // Optional
+        $item_details = $item_list;
+
+        // Optional
+        $billing_address = array(
+            'first_name'    => "Andri",
+            'last_name'     => "Litani",
+            'address'       => "Mangga 20",
+            'city'          => "Jakarta",
+            'postal_code'   => "16602",
+            'phone'         => "081122334455",
+            'country_code'  => 'IDN'
+        );
+
+        // Optional
+        $shipping_address = array(
+            'first_name'    => "Obet",
+            'last_name'     => "Supriadi",
+            'address'       => "Manggis 90",
+            'city'          => "Jakarta",
+            'postal_code'   => "16601",
+            'phone'         => "08113366345",
+            'country_code'  => 'IDN'
+        );
+
+        // Optional
+        $customer_details = array(
+            'first_name'    => "Andri",
+            'last_name'     => "Litani",
+            'email'         => "andri@litani.com",
+            'phone'         => "081122334455",
+            'billing_address'  => $billing_address,
+            'shipping_address' => $shipping_address
+        );
+
+        // Optional, remove this to display all available payment methods
+        $enable_payments = array();
+
+        // Fill transaction details
+        $transaction = array(
+            'enabled_payments' => $enable_payments,
+            'transaction_details' => $transaction_details,
+            'customer_details' => $customer_details,
+            'item_details' => $item_details,
+        );
+        // return $transaction;
+        try {
+            $snapToken = Snap::getSnapToken($transaction);
+            return response()->json($snapToken);
+            // return ['code' => 1 , 'message' => 'success' , 'result' => $snapToken];
+        } catch (\Exception $e) {
+            dd($e);
+            return ['code' => 0 , 'message' => 'failed'];
+        }
+
+    }
+
+
     /*public function search(Request $request)
     {
         try{
