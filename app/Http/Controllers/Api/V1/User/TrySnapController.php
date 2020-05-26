9e8301cd5a6a6f686b156ec89c4ff0ef47e880a1
helfanzananda <elvannnd@gmail.com>
2020-05-26 15:55:32 +0700

    coba midtrans

--- app/Http/Controllers/Api/V1/User/TrySnapController.php
+++ app/Http/Controllers/Api/V1/User/TrySnapController.php
@@ -26,9 +26,9 @@ class TrySnapController extends Controller
         $orders = $request->all();
 
         $gross_amout = 0;
-        foreach ($orders as $key => $order){
+        foreach ($orders as $order){
             array_push($result, $order);
-            $gross_amout += $order[$key]['price'] * $order[$key]['quantity'];
+            $gross_amout += $order['price'] * $order['quantity'];
         }
         $payload = [
             'transaction_details' => [
