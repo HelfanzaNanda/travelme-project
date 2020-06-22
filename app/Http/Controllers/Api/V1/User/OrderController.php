4ce9a9fca74a3a9452c9cbcfe11cc2b4e35996ba
HelfanzaNanda <elvannnd@gmail.com>
2020-06-22 13:08:48 +0700

    snap token

--- app/Http/Controllers/Api/V1/User/OrderController.php
+++ app/Http/Controllers/Api/V1/User/OrderController.php
@@ -119,9 +119,9 @@ class OrderController extends Controller
 
         try {
             $snapToken = Snap::getSnapToken($payload);
-            Order::where('id', $orders[0]['id'])->update([
-                'snap_token' => $snapToken
-            ]);
+            $snapOrder = Order::where('id', $order->id)->first();
+            $snapOrder->snap_token = $snapToken;
+            $snapOrder->update();
             return response()->json($snapToken);
         } catch (\Exception $exception) {
             return response()->json($exception->getMessage());
