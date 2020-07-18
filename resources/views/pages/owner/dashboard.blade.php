4c4fe07da3fd103230eae02a0b82fecea6ac0eb4
HelfanzaNanda <elvannnd@gmail.com>
2020-07-18 08:21:49 +0700

    finishing

--- resources/views/pages/owner/dashboard.blade.php
+++ resources/views/pages/owner/dashboard.blade.php
@@ -3,9 +3,6 @@
 <div class="row page-titles">
     <div class="col-md-6 col-8 align-self-center">
         <h3 class="text-themecolor mb-0 mt-0">Dashboard</h3>
-        <ol class="breadcrumb">
-            <li class="breadcrumb-item"><a href="javascript:void(0)">Dashboard</a></li>
-        </ol>
     </div>
 </div>
 
