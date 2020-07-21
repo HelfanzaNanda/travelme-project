2e6dc974b87bd3810c26bdf4c7b438cb5cb0d251
HelfanzaNanda <elvannnd@gmail.com>
2020-07-22 06:13:53 +0700

    update profile

--- app/Http/Controllers/Api/V1/Driver/DriverController.php
+++ app/Http/Controllers/Api/V1/Driver/DriverController.php
@@ -8,6 +8,7 @@ use App\Order;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Facades\Validator;
 
 class DriverController extends Controller
