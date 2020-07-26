302109333ddfb22ae75af95f7d9a404c3c0ce26f
ikhwanudin23 <ikhwanudin2309@gmail.com>
2020-07-26 13:25:34 +0700

    forgot password

--- app/Http/Controllers/Api/V1/User/Auth/ForgotPasswordController.php
+++ app/Http/Controllers/Api/V1/User/Auth/ForgotPasswordController.php
@@ -12,11 +12,19 @@ class ForgotPasswordController extends Controller
 
     protected function sendResetLinkResponse(Request $request, $response)
     {
-        return response(['message' => trans($response)], 200);
+        return response([
+            'message' => trans($response),
+            'status' => true,
+            'data' => (object)[]
+            ], 200);
     }
 
     protected function sendResetLinkFailedResponse(Request $request, $response)
     {
-        return response(['message' => trans($response)], 422);
+        return response([
+            'message' => trans($response),
+            'status' => true,
+            'data' => (object)[]
+            ], 422);
     }
 }
